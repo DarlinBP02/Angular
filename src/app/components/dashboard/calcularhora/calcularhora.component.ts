fd543cf7600dd57186c2b8e3d7646008ef0ec77d
DarlinBP02 <darlinbravo02@outlook.com>
2023-01-02 18:54:49 -0500

    cambios

--- src/app/components/dashboard/calcularhora/calcularhora.component.ts
+++ src/app/components/dashboard/calcularhora/calcularhora.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-calcularhora',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./calcularhora.component.css']
 })
 export class CalcularhoraComponent {
+
+   calcular(){
+    let val1: any = document.querySelector('#horas');
+    parseFloat(val1);
+    let val2: any = document.querySelector('#dias');
+    parseFloat(val2);
+
+    
+    let val3=( (val1 * 60) / val2);
+    if(val3>60){
+      let minutos = val3 % 60;
+      let horas = (val3 - minutos) / 60;
+      alert(horas+ ":" + minutos+ " horas por dia");
+    }else{
+      alert(val3+ " minutos por dia");
+    }
+    }
+    
+    
+    
+   calcula(){
+    this.calcular();
+    }
   
 }
+
